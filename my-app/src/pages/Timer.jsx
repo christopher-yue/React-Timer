a5d4011a0c111da9ebf923f7e90830ef620fd886
chris <cy.chrisyue@gmail.com>
2022-12-04 18:07:32 -0500

    updated timer

--- my-app/src/pages/Timer.jsx
+++ my-app/src/pages/Timer.jsx
@@ -2,85 +2,70 @@ import { useState, useEffect } from "react";
 import React from "react";
 
 export function Timer() {
-  const [time, setTime] = useState({ ms: 0, s: 0, m: 0, h: 0 });
+  const [time, setTime] = React.useState(0);
+  const [status, setStatus] = React.useState(0);
   const [interv, setInterv] = useState();
-  const [status, setStatus] = useState(0);
-
-  const h = () => {
-    if (time.h === 0) {
-      return "";
-    } else {
-      return <span>{time.h >= 10 ? h : "0" + time.h}</span>;
-    }
-  };
 
   /*
-  not started = 0
-  started = 1
-  paused = 2
+   0 = off
+   1 = on
+   2 = paused
   */
 
-  var updateMs = time.ms,
-    updateS = time.s,
-    updateM = time.m,
-    updateH = time.h;
-
   const run = () => {
-    if (updateM === 60) {
-      updateH++;
-      updateM = 0;
-    }
-    if (updateS === 60) {
-      updateM++;
-      updateS = 0;
-    }
-    if (updateMs === 100) {
-      updateS++;
-      updateMs = 0;
-    }
-    updateMs++;
-    return setTime({ ms: updateMs, s: updateS, m: updateM, h: updateH });
+    return setTime((prevTime) => prevTime + 1);
   };
 
   useEffect(() => {
+    let interval = null;
+
     const start = (e) => {
-      run();
       setInterv(setInterval(run, 10));
       setStatus(1);
+      console.log("start");
     };
+
     const stop = (e) => {
       clearInterval(interv);
       setStatus(2);
+      console.log("stop");
     };
+
     const reset = (e) => {
       clearInterval(interv);
-      setTime({ ms: 0, s: 0, m: 0, h: 0 });
+      setTime(0);
       setStatus(0);
+      console.log("reset");
     };
+
     if (status === 0) {
       document.body.addEventListener("touchend", start);
       return () => document.body.removeEventListener("touchend", start);
-    }
-    if (status === 1) {
-      document.body.addEventListener("touchend", stop);
+    } else if (status === 1) {
+      document.body.addEventListener("click", stop);
       return () => document.body.removeEventListener("touchend", stop);
-    }
-    if (status === 2) {
-      document.body.addEventListener("touchend", reset);
+    } else if (status === 2) {
+      document.body.addEventListener("click", reset);
       return () => document.body.removeEventListener("touchend", reset);
     }
+    return () => clearInterval(interval);
   });
 
   return (
     <div>
-      {h()}
+      <span>
+        {Math.floor((time / 6000) % 60) >= 10
+          ? Math.floor((time / 6000) % 60)
+          : "0" + Math.floor((time / 6000) % 60)}
+      </span>
       &nbsp;&nbsp;
-      <span>{time.m >= 10 ? time.m : "0" + time.m}</span>
+      <span>
+        {Math.floor((time / 100) % 60) >= 10
+          ? Math.floor((time / 100) % 60)
+          : "0" + Math.floor((time / 100) % 60)}
+      </span>
       &nbsp;&nbsp;
-      <span>{time.s >= 10 ? time.s : "0" + time.s}</span>
-      &nbsp;&nbsp;
-      <span>{time.ms >= 10 ? time.ms : "0" + time.m}</span>
-      &nbsp;&nbsp;
+      <span>{time % 100 >= 10 ? time % 100 : "0" + (time % 100)}</span>
     </div>
   );
 }
